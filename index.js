ead34e11d6aab707367c6ad7b5be8d7cd8735318
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-26 22:16:51 -0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    'no-console': 1,
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
